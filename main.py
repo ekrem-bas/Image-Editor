affedf860b030843e9117f04c6e00937da5a859a
Ekrem <ekrembas03@outlook.com>
2024-06-08 18:06:12 +0300

    added resize image, save
    changes on draw method

--- main.py
+++ main.py
@@ -1,76 +1,98 @@
 import tkinter as tk
-from tkinter import ttk
-from tkinter import filedialog
-from tkinter import colorchooser
-from PIL import Image, ImageOps, ImageFilter, ImageTk, Image, ImageDraw
+from tkinter import filedialog, colorchooser
+from PIL import Image, ImageTk, ImageDraw, ImageOps, ImageFilter, ImageDraw2
 
-# screen
+# Ekran
 root = tk.Tk()
 root.geometry("1000x600")
 root.title("Image Editor")
 
-
-# default pen color
+# Varsayılan kalem rengi ve boyutu
 pen_color = "Black"
-# default pen size
 pen_size = 5
-# file path
+
+# Global değişkenler
 file_path = None
+original_image = None
+filtering_image = original_image
+image_tk = None
+draw = None
 
-# open image function
+def resize_image(image, target_width, target_height):
+    # Aspect ratio'yu koruyarak resmi yeniden boyutlandır
+    original_width, original_height = image.size
+    ratio = min(target_width / original_width, target_height / original_height)
+    new_width = int(original_width * ratio)
+    new_height = int(original_height * ratio)
+    resized_image = image.resize((new_width, new_height), Image.LANCZOS)
+    return resized_image
+
 def open_image():
-        global file_path
-        file_path = filedialog.askopenfilename(filetypes=[("JPEG Files" , ".jpeg") , ("PNG Files", ".png")])
-        image = Image.open(file_path)
-        width, height = int(image.width // 2), int(image.height // 2)
-        if width > canvas.winfo_width() or height > canvas.winfo_height():
-                canvas.config(width= image.width, height= image.height)
-                image = image.resize(size= (canvas.winfo_width(), canvas.winfo_height()), resample= Image.Resampling.LANCZOS)
-                
-        else:
-                image = image.resize(size= (width, height), resample= Image.Resampling.LANCZOS) # antialias
-                canvas.config(width= image.width, height= image.height)        
-        
-        # adjust the root screen with the height and width of the image
-        root.geometry(f"{image.width + 237}x{image.height}")
-        image = ImageTk.PhotoImage(image)
-        canvas.image = image
-        canvas.create_image(0, 0, image = image, anchor = "nw")
+    global original_image, image_tk
 
-# draw function
-draw_flag = False
-def draw():
-        global draw_flag
-        draw_flag = not draw_flag
-        if draw_flag:
-                canvas.bind("<B1-Motion>", start_draw)
-        else:
-                canvas.unbind("<B1-Motion>")   
-        
+    file_path = filedialog.askopenfilename(filetypes=[("JPEG Files", "*.jpeg"), ("PNG Files", "*.png")])
+    if not file_path:
+        return
 
-def start_draw(event):
-                x1 = (event.x - pen_size)
-                y1 = (event.y - pen_size)
-                x2 = (event.x + pen_size)
-                y2 = (event.y + pen_size)
-                canvas.create_oval(x1, y1, x2, y2, fill= pen_color, outline= "")   
+    original_image = Image.open(file_path)
+    
+    canvas_width = canvas.winfo_width()
+    canvas_height = canvas.winfo_height()
 
-# change color function        
-def change_color():
-        global pen_color
-        pen_color = colorchooser.askcolor(title= "Select Pen Color")[1]
-        
-# change pen size function
-def change_pen_size(event = None):
-        global pen_size
-        pen_size = int (ps_spin_box.get())
+    resized_image = resize_image(original_image, canvas_width, canvas_height)
+    image_tk = ImageTk.PhotoImage(resized_image)
+    global drawing_image
+    drawing_image = original_image
 
-# clear canvas function
-def clear_canvas():
+    canvas.delete("all")
+    canvas.create_image(canvas_width // 2, canvas_height // 2, anchor="center", image=image_tk)
+    canvas.image = image_tk  # Referans tutarak görüntünün kaybolmasını engelle
+
+def on_canvas_resized(event):
+    if original_image:
+        canvas_width = event.width
+        canvas_height = event.height
+
+        resized_image = resize_image(original_image, canvas_width, canvas_height)
+        image_tk = ImageTk.PhotoImage(resized_image)
+
         canvas.delete("all")
-        canvas.create_image(0, 0, image = canvas.image, anchor = "nw")     
+        canvas.create_image(canvas_width // 2, canvas_height // 2, anchor="center", image=image_tk)
+        canvas.image = image_tk  # Referans tutarak görüntünün kaybolmasını engelle
 
-# filter window
+def save_file():
+    global drawing_image
+    save_file_path = filedialog.asksaveasfilename(defaultextension=".png", filetypes=[("PNG files", "*.png"), ("JPEG files", "*.jpeg")])
+    if save_file_path and drawing_image:
+        drawing_image.save(save_file_path)
+
+def draw_on_image(event):
+    global drawing_image
+    x, y = event.x, event.y
+    x1, y1 = (x - pen_size), (y - pen_size)
+    x2, y2 = (x + pen_size), (y + pen_size)
+    canvas.create_oval(x1, y1, x2, y2, fill=pen_color, outline=pen_color)
+    draw = ImageDraw.Draw(drawing_image)
+    draw.ellipse([x1 , y1, x2, y2], fill=pen_color, outline=pen_color)
+
+def toggle_draw():
+    if canvas.bind("<B1-Motion>"):
+        canvas.unbind("<B1-Motion>")
+    else:
+        canvas.bind("<B1-Motion>", draw_on_image)
+
+def change_color():
+    global pen_color
+    pen_color = colorchooser.askcolor(title="Select Pen Color")[1]
+
+def change_pen_size(event=None):
+    global pen_size
+    pen_size = int(ps_spin_box.get())
+
+def clear_canvas():
+    canvas.delete("all")
+    canvas.create_image(canvas.winfo_width() // 2, canvas.winfo_height() // 2, anchor="center", image=image_tk)
+    
 def open_filter_menu():
         filter_menu = tk.Toplevel(root)
         filter_menu.geometry("250x350")
@@ -78,7 +100,7 @@ def open_filter_menu():
         filter_menu.config(bg= "white")
         
         # black and white button
-        black_and_white_button = tk.Button(master= filter_menu, text= "Sharpen", command= lambda : apply_filter("Black and White"), highlightbackground= "white", fg= "black")
+        black_and_white_button = tk.Button(master= filter_menu, text= "Black and White", command= lambda : apply_filter("Black and White"), highlightbackground= "white", fg= "black")
         black_and_white_button.pack(pady= 15)
         # blur button
         blur_button = tk.Button(master= filter_menu, text= "Blur", command= lambda : apply_filter("Blur"), highlightbackground= "white", fg= "black")
@@ -96,78 +118,74 @@ def open_filter_menu():
         clear_button = tk.Button(master= filter_menu, text= "Clear", command= lambda : apply_filter("Clear"), highlightbackground= "white", fg= "black")
         clear_button.pack()
         
-# TODO : Add filters  (clear button is completed) / continue to video from 39:11
-# TODO : look at imageOps contain function (something about resizing, it can be used for the crop operation)
-# apply filter function
+
 def apply_filter(filter_):
-        image = Image.open(file_path)
-        width, height = int(image.width / 2), int(image.height / 2)
-        image = image.resize(size= (width, height), resample= Image.Resampling.LANCZOS) # antialias
+        global filtering_image
+        temp_image = original_image
+        image = resize_image(original_image, canvas.winfo_width(), canvas.winfo_height())
         if filter_ == "Black and White":
                 image = ImageOps.grayscale(image)
+                temp_image = ImageOps.grayscale(original_image)
         elif filter_ == "Blur":
                 image = image.filter(ImageFilter.BLUR)
+                temp_image = temp_image.filter(ImageFilter.BLUR)
         elif filter_ == "Sharpen":
                 image = image.filter(ImageFilter.SHARPEN)
+                temp_image = temp_image.filter(ImageFilter.SHARPEN)
         elif filter_ == "Smooth":
                 image = image.filter(ImageFilter.SMOOTH)
+                temp_image = temp_image.filter(ImageFilter.SMOOTH)
         elif filter_ == "Emboss":
                 image = image.filter(ImageFilter.EMBOSS)
+                temp_image = temp_image.filter(ImageFilter.EMBOSS)
         else:
-                image = image
-        image2 = ImageTk.PhotoImage(image)
-        canvas.image = image2
-        canvas.create_image(0, 0, image = image2, anchor = "nw")  
-        
+                image = resize_image(original_image, canvas.winfo_width(), canvas.winfo_height())
+        image_tk = ImageTk.PhotoImage(image)
+        canvas.delete("all")
+        canvas.create_image(canvas.winfo_width() // 2, canvas.winfo_height() // 2, anchor="center", image=image_tk)
+        canvas.image = image_tk
+        filtering_image = temp_image
 
-# left part of the screen - left frame
+# Sol çerçeve
 left_frame = tk.Frame(root, width=200, height=800, bg="white")
-left_frame.pack(side="left", fill= "y")
+left_frame.pack(side="left", fill="y")
 
-# right part of the screen - right frame
-right_frame = tk.Frame(root, width= 1000, height= 800, bg= "white")
-right_frame.pack(fill= "both", expand= True)
+# Sağ çerçeve
+right_frame = tk.Frame(root, width=1000, height=800, bg="white")
+right_frame.pack(fill="both", expand=True)
 
-# canvas
-canvas = tk.Canvas(right_frame, width= 200, height= 800, bg= "grey", highlightthickness= 0)
-canvas.pack(fill= "both", expand= True)
+# Canvas
+canvas = tk.Canvas(right_frame, width=200, height=800, bg="grey", highlightthickness=0)
+canvas.pack(fill="both", expand=True)
 
-# image button
-image_button = tk.Button(master= left_frame, text= "Add Image", highlightbackground="white", command= open_image)
-image_button.pack(pady= 15, padx= 15)
+# Menü
+image_button = tk.Button(master=left_frame, text="Add Image", highlightbackground="white", command=open_image)
+image_button.pack(pady=15, padx=15)
 
-# color button
-color_button = tk.Button(master= left_frame, text= "Change Pen Color", highlightbackground= "white", command= change_color)
-color_button.pack(padx= 15)
+color_button = tk.Button(master=left_frame, text="Change Pen Color", highlightbackground="white", command=change_color)
+color_button.pack(padx=15)
 
-# pen size 
 pen_size_frame = tk.Frame(master=left_frame, bg="white")
 pen_size_frame.pack(pady=15)
 
 spinbox_label = tk.Label(pen_size_frame, text="Change Pen Size", bg="white", fg="black")
 spinbox_label.pack()
 
-ps_spin_box = tk.Spinbox(pen_size_frame, from_=1, to=10, command=change_pen_size, textvariable=tk.StringVar(value="5"), highlightbackground= "white", background= "white", foreground="black")
-ps_spin_box.pack(padx= 15)
-# when user press enter on spinbox it will change the pen size
+ps_spin_box = tk.Spinbox(pen_size_frame, from_=1, to=10, command=change_pen_size, textvariable=tk.StringVar(value="5"), highlightbackground="white", background="white", foreground="black")
+ps_spin_box.pack(padx=15)
 ps_spin_box.bind("<Return>", change_pen_size)
 
-
-draw_button = tk.Button(master= pen_size_frame, text= "Toggle Draw", highlightbackground= "white", command= draw)
+draw_button = tk.Button(master=pen_size_frame, text="Toggle Draw", highlightbackground="white", command=toggle_draw)
 draw_button.pack()
 
-# clear button
-clear_canvas_button = tk.Button(master= left_frame, text= "Clear", highlightbackground= "white", command= clear_canvas)
+clear_canvas_button = tk.Button(master=left_frame, text="Clear", highlightbackground="white", command=clear_canvas)
 clear_canvas_button.pack()
 
-# drawing
-# TODO: add button to draw on picture
-
-
+save_button = tk.Button(master=left_frame, text="Save", highlightbackground="white", command=save_file)
+save_button.pack()
 
 # filter menu
 filter_button = tk.Button(left_frame, text= "Select a filter", highlightbackground= "white", command= open_filter_menu)
-filter_button.pack(pady= 15)
-
+filter_button.pack()
 
 root.mainloop()
